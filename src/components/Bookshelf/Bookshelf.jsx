c5f138fefea76d604aa55be595991e2115e49d98
Mujtaba1i <unmujt@gmail.com>
2025-12-23 14:04:03 +0300

    quick commit

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -30,15 +30,15 @@ function Bookshelf(){
                 <form onSubmit={handleSubmit}>
 
                     <label htmlFor="title">title: </label>
-                    <input name="title" id="title" type="text" value={newBook.title} onChange={handleInputChange} /><br /><br />                    
+                    <input name="title" id="title" type="text" value={newBook.title} onChange={handleInputChange} /><br />                    
                     
                     <label htmlFor="auther">auther: </label>
-                    <input name="auther" id="auther" type="text" value={newBook.auther} onChange={handleInputChange} /><br /><br />
+                    <input name="auther" id="auther" type="text" value={newBook.auther} onChange={handleInputChange} /><br />
                     <button>Add Book</button>
                 </form>
             </div>
             <div>
-                {books.map((oneBook)=><div className="bookCard"> title: {oneBook.title} auther: {oneBook.auther}</div>)}
+                {books.map((oneBook)=><div className="bookCard"> <h2> {oneBook.title} </h2> by: {oneBook.auther}</div>)}
             </div>
         </div>
         </>
