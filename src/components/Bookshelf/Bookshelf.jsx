60f4649ae7e082b0865a7e59c7ddf9a8e226163b
Mujtaba1i <unmujt@gmail.com>
2025-12-23 12:30:00 +0300

    quick commit

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+function Bookshelf(){
+
+    const [books, setBooks] = useState([])
+    const [newBook, setNewBook] = useState({
+        title:'',
+        auther:''
+    })
+
+    function handleSubmit(event){
+        event.preventDefault()
+        setBooks([...books,newBook])
+        setNewBook({
+            title:'',
+            auther:''
+        })
+        
+    }
+    
+    function handleInputChange(event){
+        setNewBook({...newBook,[event.target.name]:event.target.value})
+    }
+
+    return(
+        <>
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                <form onSubmit={handleSubmit}>
+
+                    <label htmlFor="title">title: </label>
+                    <input name="title" id="title" type="text" value={newBook.title} onChange={handleInputChange} /><br /><br />                    
+                    
+                    <label htmlFor="auther">auther: </label>
+                    <input name="auther" id="auther" type="text" value={newBook.auther} onChange={handleInputChange} /><br /><br />
+                    <button>Add Book</button>
+                </form>
+            </div>
+            <div>
+                {books.map((oneBook)=><div className="bookCard"> title: {oneBook.title} auther: {oneBook.auther}</div>)}
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Bookshelf
